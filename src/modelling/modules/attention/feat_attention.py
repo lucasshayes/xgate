e27a14e44648b4e121e9d28e5af0dfc93708641f
lucasshayes <39037513+lucasshayes@users.noreply.github.com>
2025-08-11 14:51:26 +0100

    Update feat_attention.py

--- src/modelling/modules/attention/feat_attention.py
+++ src/modelling/modules/attention/feat_attention.py
@@ -10,9 +10,10 @@ def feat_attention(inputs):
     Returns:
         Tensor: Output tensor with applied feature attention.
     """
-    # Avg pool across the feature dimension
+    # Pools over and reduces temporal dimension
     avg_pool = layers.GlobalAveragePooling1D(name="feat_attention_avg_pool")(inputs)
     # Softmax to get attention weights
+    # Could instead use SENet style learnable weights using 2 dense layers with r ratio. A/B test accuracy.
     attn_weights = layers.Softmax(axis=-1, name="feat_attention_weights")(avg_pool)
     # Reshape to match the input shape
     attn_weights = layers.Reshape((1, inputs.shape[-1]), name="feat_attention_reshape")(attn_weights)
